bb6671dc7b047870e081a616efc914e8b9f022d8
Madhumitha E <madhumithaelavarasan419@gmail.com>
2026-03-12 10:05:08 +0530

    Students Result Checker

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-function CheckResult(){
-let StudentName="Madhumitha";
-let mark=96;
-let isPresent=true;
+//Global array
+let students = [];
+
+// Function to add student
+function addStudent(name, marks) {
+
+    students.push({
+        studentName: name,
+        studentMarks: marks
+    });
+
+}
+
+// Students add
+addStudent("Madhumitha", [80,75,90]);
+addStudent("Arun", [60,55,70]);
+addStudent("Priya", [40,45,50]);
+
+// Function to check result
+function checkResult(){
+
+let resultText = "";
+
+for(let i=0;i<students.length;i++){
+
+let student = students[i];
+let marks = student.studentMarks;
+
+let total = 0;
+
+for(let j=0;j<marks.length;j++){
+total += marks[j];
+}
+
+let average = total / marks.length;
 
 let result;
-if (mark>=50){
-    result="Pass";
-}else{
-    result="Fail";
+
+if(average >= 50){
+result = "PASS";
 }
-let grade;
-
-switch(true){
-    case(mark>=90):
-    grade="A+";
-    break;
-
-    case(mark>=75):
-    grade="A";
-    break;
-
-    case(mark>=60):
-    grade="B";
-    break;
-
-    case(mark>=50):
-    grade="C";
-    break;
-
-    default:
-        grade="Fail";
+else{
+result = "FAIL";
 }
-document.getElementById("output").innerHTML=
-"Name:"+ StudentName +
-"<br>Mark:"+ mark +
-"<br>Result:"+ result +
-"<br>Grade:"+ grade ;
+
+resultText += "Name: " + student.studentName + "<br>";
+resultText += "Average: " + average + "<br>";
+resultText += "Result: " + result + "<br>";
+resultText += "-------------------<br>";
+
+}
+
+// Show in live page
+document.getElementById("output").innerHTML = resultText;
+
 }
